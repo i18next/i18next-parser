2f3073b88393aed86d4b4c2f011fdadbb7ff5608
Luc Heinrich <luc@honk-honk.com>
2018-02-08 22:43:40 +0100

    Allow indentation level to be specified as a cli parameter

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ function Parser(options, transformConfig) {
     this.keepRemoved        = options.keepRemoved;
     this.ignoreVariables    = options.ignoreVariables || false;
     this.defaultValues      = options.defaultValues || false;
+    this.indentation        = options.indentation || 2;
 
     ['functions', 'locales'].forEach(function( attr ) {
         if ( (typeof self[ attr ] !== 'object') || ! self[ attr ].length ) {
@@ -289,7 +290,7 @@ Parser.prototype._flush = function(done) {
             mergedTranslationsFile = new File({
               path: namespacePath,
               base: base,
-              contents: new Buffer( JSON.stringify( mergedTranslations.new, null, 2 ) )
+              contents: new Buffer( JSON.stringify( mergedTranslations.new, null, this.indentation ) )
             });
             this.emit( 'writing', namespacePath );
             self.push( mergedTranslationsFile );
@@ -298,7 +299,7 @@ Parser.prototype._flush = function(done) {
                 mergedOldTranslationsFile = new File({
                     path: namespaceOldPath,
                     base: base,
-                    contents: new Buffer(JSON.stringify(mergedTranslations.old, null, 2))
+                    contents: new Buffer(JSON.stringify(mergedTranslations.old, null, this.indentation))
                 });
                 this.emit( 'writing', namespaceOldPath );
                 self.push( mergedOldTranslationsFile );
