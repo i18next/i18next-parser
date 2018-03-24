1001ac1f4975f4e041fc616957cf81acde4f1724
coyotte508 <coyotte508@gmail.com>
2018-03-24 15:40:01 +0100

    Fix react test to have valid jsx

--- test/templating/react.jsx
+++ test/templating/react.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { translate } from 'react-i18next'
+import { translate, Trans, Interpolate } from 'react-i18next'
 
 class Test extends React.Component {
   render () {
