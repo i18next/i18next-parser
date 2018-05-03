28fd34961835bb457d56cf059d7c7f5a0229b97f
Than Hutchins <mynockspit@gmail.com>
2018-05-03 10:17:40 -0700

    moved JSX parsing to acorn-jsx (is now a side-effect of JS parsing)

--- test/lexers/jsx-lexer.test.js
+++ test/lexers/jsx-lexer.test.js
@@ -1,12 +1,12 @@
 import { assert } from 'chai'
-import JsxLexer from '../../src/lexers/jsx-lexer'
+import JavascriptLexer from '../../src/lexers/javascript-lexer'
 
 describe('JsxLexer', () => {
   describe('extractInterpolate', () => {
     it('extracts keys from i18nKey attributes', (done) => {
-      const Lexer = new JsxLexer()
+      const Lexer = new JavascriptLexer()
       const content = '<Interpolate i18nKey="first" />'
-      assert.deepEqual(Lexer.extractInterpolate(content), [
+      assert.deepEqual(Lexer.extract(content), [
         { key: 'first' }
       ])
       done()
@@ -15,40 +15,58 @@ describe('JsxLexer', () => {
 
   describe('Trans', () => {
     it('extracts keys from i18nKey attributes from closing tags', (done) => {
-      const Lexer = new JsxLexer()
+      const Lexer = new JavascriptLexer()
       const content = '<Trans i18nKey="first" count={count}>Yo</Trans>'
-      assert.deepEqual(Lexer.extractTrans(content), [
+      assert.deepEqual(Lexer.extract(content), [
+        { key: 'first', defaultValue: 'Yo' }
+      ])
+      done()
+    })
+    
+    it('extracts keys from user-defined key attributes from closing tags', (done) => {
+      const Lexer = new JavascriptLexer({ attr: "myIntlKey" })
+      const content = '<Trans myIntlKey="first" count={count}>Yo</Trans>'
+      assert.deepEqual(Lexer.extract(content), [
         { key: 'first', defaultValue: 'Yo' }
       ])
       done()
     })
 
     it('extracts keys from i18nKey attributes from self-closing tags', (done) => {
-      const Lexer = new JsxLexer()
+      const Lexer = new JavascriptLexer()
       const content = '<Trans i18nKey="first" count={count} />'
-      assert.deepEqual(Lexer.extractTrans(content), [
+      assert.deepEqual(Lexer.extract(content), [
+        { key: 'first' }
+      ])
+      done()
+    })
+
+    it('extracts keys from user-defined key attributes from self-closing tags', (done) => {
+      const Lexer = new JavascriptLexer({ attr: "myIntlKey" })
+      const content = '<Trans myIntlKey="first" count={count} />'
+      assert.deepEqual(Lexer.extract(content), [
         { key: 'first' }
       ])
       done()
     })
 
     it('extracts keys from Trans elements without an i18nKey', (done) => {
-      const Lexer = new JsxLexer()
+      const Lexer = new JavascriptLexer()
       const content = '<Trans count={count}>Yo</Trans>'
-      assert.deepEqual(Lexer.extractTrans(content), [
+      assert.deepEqual(Lexer.extract(content), [
         { key: 'Yo', defaultValue: 'Yo' }
       ])
       done()
     })
 
     it('doesn\'t add a blank key for self-closing or empty tags', (done) => {
-      const Lexer = new JsxLexer()
+      const Lexer = new JavascriptLexer()
       
       const emptyTag = '<Trans count={count}></Trans>'
-      assert.deepEqual(Lexer.extractTrans(emptyTag), [])
+      assert.deepEqual(Lexer.extract(emptyTag), [])
 
       const selfClosing = '<Trans count={count}/>'
-      assert.deepEqual(Lexer.extractTrans(selfClosing), [])
+      assert.deepEqual(Lexer.extract(selfClosing), [])
 
       done()
     })
@@ -56,9 +74,9 @@ describe('JsxLexer', () => {
 
   describe('eraseTags()', () => {
     it('erases tags from content', (done) => {
-      const Lexer = new JsxLexer()
+      const Lexer = new JavascriptLexer()
       const content = '<Trans>a<b test={"</b>"}>c<c>z</c></b>{d}<br stuff={y}/></Trans>'
-      assert.equal(Lexer.eraseTags(content), 'a<1>c<1>z</1></1><2>{d}</2><3></3>')
+      assert.equal(Lexer.extract(content)[0].defaultValue, 'a<1>c<1>z</1></1><2>{d}</2><3></3>')
       done()
     })
   })
