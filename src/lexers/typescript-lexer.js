7274f70e33003713f20e54f7c13b7d1798dea198
Tanner Stirrat <tstirrat@gmail.com>
2018-09-11 12:26:27 -0400

    Move logic into the constructor

--- src/lexers/typescript-lexer.js
+++ src/lexers/typescript-lexer.js
@@ -2,24 +2,20 @@ import JsxLexer from './jsx-lexer'
 import { readFileSync } from 'fs'
 import { join } from 'path'
 
-function loadTypeScript() {
-  try {
-    return require('typescript')
-  } catch (e) {
-    throw new ParsingError(`You must install typescript to parse TypeScript files. `
-      + `Try running "yarn add typescript" or "npm install typescript"`)
-  }
-  return null
-}
-
 export default class TypescriptLexer extends JsxLexer {
   constructor(options = {}) {
     super(options)
     this.tsOptions = options.tsOptions
+    try {
+      this.typescript = require('typescript')
+    } catch (e) {
+      throw new Error(`You must install typescript to parse TypeScript files. `
+        + `Try running "yarn add typescript" or "npm install typescript"`)
+    }
   }
 
   extract(content, extension) {
-    const transpiled = loadTypeScript().transpileModule(content, {
+    const transpiled = this.typescript.transpileModule(content, {
       compilerOptions: {
         ...this.tsOptions,
         jsx: 'Preserve',
