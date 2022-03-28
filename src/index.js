8ce0a46a8c60c35751bb6f8941dec7f654b7d504
Karel Ledru <karel@ledrumathe.com>
2022-03-27 21:50:39 -0400

    Export lexers in index.js (closes #532)

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ export { default as broccoli } from './broccoli.js'
 export { default as parser } from './parser.js'
 export { default as transform } from './transform.js'
 export { default as gulp } from './transform.js'
+
+// Lexers
+export { default as BaseLexer } from './lexers/base-lexer.js'
+export { default as HandlebarsLexer } from './lexers/handlebars-lexer.js'
+export { default as HTMLLexer } from './lexers/html-lexer.js'
+export { default as JavascriptLexer } from './lexers/javascript-lexer.js'
+export { default as JsxLexer } from './lexers/jsx-lexer.js'
+export { default as VueLexer } from './lexers/vue-lexer.js'
